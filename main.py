7e166acf26d83209661c873ba3be9907d914f134
gabriellebk <gsb5225@psu.edu>
2020-09-03 14:11:15 +0000

    Another spelling mistake

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ temp = input("Enter temperature: ")
 unit = input("Enter unit in F/f or C/c: ")
 temp = float(temp)
 if (unit == "F" or unit == "f"):
-  print(f"{temp}° in Fahrenheit is equivilant to {(temp-32)*(5/9)}° Celsius.")
+  print(f"{temp}° in Fahrenheit is equivalant to {(temp-32)*(5/9)}° Celsius.")
 elif (unit == "C" or unit == "c"):
-  print(f"{temp}° in Celsius is equivalent to {(temp*1.8)+32}° Fahrenheit. ")
+  print(f"{temp}° in Celsius is equivalent to {(temp*1.8)+32}° Fahrenheit.")
 else:
   print("Invalid unit(" + unit +"). ")
-  
